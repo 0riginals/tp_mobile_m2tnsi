081ead3d4633a79fa4f8948295b1ca71216bcb39
SnowYou4Times <philippe_normand@hotmail.fr>
2020-12-22 20:26:53 +0100

    [FEAT] Ajout de la gestion de data + quelques exemples
    -Modification de la structure de assets en ajoutant un folder pictures contenant les images des Data
    -Folder Data à la racine du projet, avec quelques exemples prédéfini dans MealData.js

---
+++ Data/MealData.js
@@ -0,0 +1,51 @@
+export default data = [
+    {
+        id: 0,
+        title: 'Fondant au chocolat',
+        description: 'Un coeur en chocolat qui vous fera fondre.',
+        price: 6.59,
+        image: 'fondantChocolat.webp'
+    },
+    {
+        id: 1,
+        title: 'Cannelet Bordelais',
+        description: 'Une recette traditionnel gourmande.',
+        price: 9.20,
+        image: 'canneletBordelais.webp'
+    },
+    {
+        id: 2,
+        title: 'Gateau au chocolat',
+        description: 'Oui, celui de votre enfance!',
+        price: 14.90,
+        image: 'gateauChocolat.webp'
+    },
+    {
+        id: 3,
+        title: 'Meringue nature',
+        description: 'La meilleure façon de sublimer le sucre.',
+        price: 3.90,
+        image: 'meringue.webp'
+    },
+    {
+        id: 4,
+        title: 'Pain d\'épice',
+        description: 'Idéal avec du beurre de ferme.',
+        price: 6.50,
+        image: 'painDEpice.webp'
+    },
+    {
+        id: 5,
+        title: 'Panna Cotta',
+        description: 'La fraicheur et le fruit.',
+        price: 9.99,
+        image: 'pannaCotta.webp'
+    },
+    {
+        id: 6,
+        title: 'Cookies gourmand',
+        description: 'Des pépites, comme dans...',
+        price: 6.20,
+        image: 'cookiesChocolat.webp'
+    },
+]
